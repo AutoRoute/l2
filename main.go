de0a7ba6530acd233e8de87473834805bf3b307e
Colin Rice <colin@daedrum.net>
2014-08-12 23:57:09 -0400

    Reading both sides and dumping the MAC's. Absolutely fascinating

--- main.go
+++ main.go
@@ -2,41 +2,71 @@ package main
 
 import (
     "code.google.com/p/tuntap"
-    "log"
+    "encoding/hex"
     "fmt"
+    "log"
+    "net"
     "os/exec"
+    "unsafe"
 )
+/*
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#include <string.h>
+#include <netinet/in.h>
 
-// #include <sys/socket.h>
-// #include <linux/if_packet.h>
-// #include <linux/if_ether.h>
-// #include <linux/if_arp.h>
-// #include <string.h>
-//
-// int CreateSocket() {
-//   return socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-// };
-//
-// int SendData(int socket, int interface, char * data, size_t length) {
-//   struct sockaddr_ll socket_address;
-//   socket_address.sll_ifindex = interface;
-//   socket_address.sll_halen = ETH_ALEN;
-//   memcpy(socket_address.sll_addr, data, ETH_ALEN);
-//   return sendto(socket, data, length, 0, (struct sockaddr*)&socket_address, sizeof(socket_address));
-// };
-//
-// int ReceiveData(int socket, void* buffer) {
-//   return recvfrom(socket, buffer, ETH_FRAME_LEN, 0, NULL, NULL);
-// }
+socklen_t LLSize() {
+    return sizeof(struct sockaddr_ll);
+}
+
+int SendData(int socket, int interface, char * data, size_t length) {
+struct sockaddr_ll socket_address;
+socket_address.sll_ifindex = interface;
+socket_address.sll_halen = ETH_ALEN;
+memcpy(socket_address.sll_addr, data, ETH_ALEN);
+return sendto(socket, data, length, 0, (struct sockaddr*)&socket_address, LLSize());
+};
+*/
 import "C"
 
+func PrintPacket(name string, data []byte) {
+    log.Printf("%s: %s->%s", name,
+        hex.EncodeToString(data[0:6]),
+        hex.EncodeToString(data[6:12]))
+}
 func print_recv(tap *tuntap.Interface) {
     for {
         p, err := tap.ReadPacket()
         if err != nil {
             log.Fatal(err)
         }
-        log.Print(p)
+        PrintPacket("tap0", p.Packet)
+    }
+}
+
+func CreateListenSocket(listen_interface string) C.int {
+    sock := C.socket(C.AF_PACKET, C.SOCK_RAW, C.int(C.htons(C.ETH_P_ALL)))
+    ll_addr := C.struct_sockaddr_ll{}
+    i, err := net.InterfaceByName(listen_interface)
+    if err != nil {
+        log.Fatal(err)
+    }
+    ll_addr.sll_ifindex = C.int(i.Index)
+    C.bind(sock, (*C.struct_sockaddr)(unsafe.Pointer(&ll_addr)), C.LLSize())
+    return sock
+}
+
+func cprint_recv() {
+    sock := CreateListenSocket("wlp3s0")
+    for {
+        buffer := [1523]byte{}
+        n, err := C.recvfrom(sock, unsafe.Pointer(&buffer[0]), C.size_t(1523), 0, nil, nil)
+        if err != nil {
+            log.Fatal(err)
+        }
+        PrintPacket("wlp3s0", buffer[0:n])
     }
 }
 
@@ -69,5 +99,6 @@ func main() {
 
 
     go print_recv(fd)
+    go cprint_recv()
     fmt.Scanln();
 }
