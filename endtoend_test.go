8964091d91cd39b4ca373c94f0ebab95564574c9
Colin L. Rice <colin@daedrum.net>
2014-12-19 22:22:46 -0500

    Simplify endtoendtest

--- endtoend_test.go
+++ endtoend_test.go
@@ -7,85 +7,78 @@ import (
 	"time"
 )
 
+var (
+	dev_name = "test"
+	dev_mac  = "00:11:22:33:44:55:66"
+	dest_mac = "ff:11:22:33:44:55:66"
+)
+
 func waitForPacket(target []byte, dev PacketReader) bool {
-	done := make(chan struct{})
-	found := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			default:
-			}
-
-			pack, err := dev.ReadPacket()
-			if err != nil {
-				log.Fatal(err)
-			}
-			if bytes.Equal(target, pack) {
-				found <- struct{}{}
-				return
-			}
-			PrintPacket("desired", target)
-			PrintPacket("found", pack)
-		}
-	}()
-
 	timeout := time.After(time.Second)
+	for {
+		// Check if we're out of time
+		select {
+		case <-timeout:
+			return false
+		default:
+		}
 
-	select {
-	case <-timeout:
-		done <- struct{}{}
-		return false
-	case <-found:
-		return true
+		pack, err := dev.ReadPacket()
+		if err != nil {
+			log.Print(err)
+			return false
+		}
+		if bytes.Equal(target, pack) {
+			return true
+		}
+		PrintPacket("desired", target)
+		PrintPacket("found", pack)
 	}
 }
 
-func TestEthToTap(t *testing.T) {
-	dev_name := "test0"
-	dev_mac := "00:11:22:33:44:55:66"
-	dest_mac := "ff:11:22:33:44:55:66"
+func NewDevices() (*TapDevice, *EthDevice, error) {
 	tap, err := NewTapDevice(dev_mac, dev_name)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	eth, err := ConnectEthDevice(dev_name)
+	if err != nil {
+		tap.Close()
+		return nil, nil, err
+	}
+	return tap, eth, nil
+}
+
+func NewPacket(dest, src string) []byte {
+	data := make([]byte, 100)
+	return NewEthPacket(MAC(dest).ToBytes(), MAC(src).ToBytes(), 1, data)
+}
+
+func TestEthToTap(t *testing.T) {
+	tap, eth, err := NewDevices()
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer tap.Close()
-
-	eth_tap, err := ConnectEthDevice(dev_name)
+	packet := NewPacket(dest_mac, dev_mac)
+	err = eth.WritePacket(packet)
 	if err != nil {
 		t.Fatal(err)
 	}
-	data := make([]byte, 100)
-	packet := NewEthPacket(MAC(dest_mac).ToBytes(), MAC(dev_mac).ToBytes(), 1, data)
-	err = eth_tap.WritePacket(packet)
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	waitForPacket(packet, tap)
 }
 
 func TestTapToEth(t *testing.T) {
-	dev_name := "test1"
-	dev_mac := "00:11:22:33:44:55:66"
-	dest_mac := "ff:11:22:33:44:55:66"
-	tap, err := NewTapDevice(dev_mac, dev_name)
+	tap, eth, err := NewDevices()
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer tap.Close()
-
-	eth_tap, err := ConnectEthDevice(dev_name)
-	if err != nil {
-		t.Fatal(err)
-	}
-	data := make([]byte, 100)
-	packet := NewEthPacket(MAC(dest_mac).ToBytes(), MAC(dev_mac).ToBytes(), 1, data)
+	packet := NewPacket(dest_mac, dev_mac)
 	err = tap.WritePacket(packet)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	waitForPacket(packet, eth_tap)
+	waitForPacket(packet, eth)
 }
