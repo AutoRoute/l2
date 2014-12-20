c98d029db89519dfd54c38f410b48256ed4b40f7
Colin L. Rice <colin@daedrum.net>
2014-12-19 21:51:48 -0500

    Cleanup eth_packet so the anmes are a little cleaner and the packet check is explicit

--- eth_packet.go
+++ eth_packet.go
@@ -9,34 +9,34 @@ import (
 type EthPacket []byte
 
 func NewEthPacket(destination, source []byte, protocol uint16, data []byte) []byte {
-	packet := make([]byte, 12+2+len(data))
-	copy(packet[0:6], destination)
-	copy(packet[6:12], source)
-	binary.BigEndian.PutUint16(packet[12:14], protocol)
-	copy(packet[14:], data)
-	return packet
+	p := make([]byte, 12+2+len(data))
+	copy(p[0:6], destination)
+	copy(p[6:12], source)
+	binary.BigEndian.PutUint16(p[12:14], protocol)
+	copy(p[14:], data)
+	return p
 }
 
-func (data EthPacket) Destination() []byte {
-	return data[0:6]
+func (p EthPacket) Destination() []byte {
+	return p[0:6]
 }
 
-func (data EthPacket) Source() []byte {
-	return data[6:12]
+func (p EthPacket) Source() []byte {
+	return p[6:12]
 }
 
-func (data EthPacket) Type() []byte {
-	protocol := int(binary.BigEndian.Uint16(data[12:14]))
+func (p EthPacket) checkType() {
+	protocol := int(binary.BigEndian.Uint16(p[12:14]))
 	if protocol < 1536 {
-		log.Print("Packet protocol is ", hex.EncodeToString(data[12:14]))
+		log.Print("This packet appears to not be a 802.3ad packet which is unsupported. The protocol is ", hex.EncodeToString(p[12:14]))
 	}
-	return data[12:14]
+}
+func (p EthPacket) Type() []byte {
+	p.checkType()
+	return p[12:14]
 }
 
-func (data EthPacket) TypeInt() int {
-	protocol := int(binary.BigEndian.Uint16(data[12:14]))
-	if protocol < 1536 {
-		log.Print("Packet protocol is ", hex.EncodeToString(data[12:14]))
-	}
-	return protocol
+func (p EthPacket) TypeInt() int {
+	p.checkType()
+	return int(binary.BigEndian.Uint16(p[12:14]))
 }
