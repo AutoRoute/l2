3a71ce5ff3dfbdf6051bb47d54aca57ebaa7fb26
Colin L. Rice <colin@daedrum.net>
2015-01-19 14:25:31 -0500

    Add socket devices

---
+++ socket_dev.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+)
+
+type SocketDevice struct {
+	io.ReadWriteCloser
+}
+
+func NewListener(address string) (*SocketDevice, error) {
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	c, err := ln.Accept()
+	return &SocketDevice{c}, err
+}
+
+func NewDialer(address string) (*SocketDevice, error) {
+	c, err := net.Dial("tcp", address)
+	return &SocketDevice{c}, err
+}
+
+func (s *SocketDevice) WritePacket(data []byte) error {
+	err := binary.Write(s, binary.BigEndian, int16(len(data)))
+	if err != nil {
+		return err
+	}
+	for written := 0; written < len(data); {
+		n, err := s.Write(data[written:])
+		if err != nil {
+			return err
+		}
+		written += n
+	}
+	return nil
+}
+
+func (s *SocketDevice) ReadPacket() ([]byte, error) {
+	var size int16
+	err := binary.Read(s, binary.BigEndian, &size)
+	if err != nil {
+		return nil, err
+	}
+	buffer := make([]byte, size)
+	_, err = io.ReadFull(s, buffer)
+	return buffer, err
+}
---
+++ socket_dev_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSocketDevice(t *testing.T) {
+	sync := make(chan struct{})
+	go func() {
+		sync <- struct{}{}
+		c, err := NewListener("127.0.0.1:9000")
+		if err != nil {
+			t.Fatal(err)
+		}
+		p, err := c.ReadPacket()
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = c.WritePacket(p)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+	<-sync
+	c, err := NewDialer("127.0.0.1:9000")
+	if err != nil {
+		t.Fatal(err)
+	}
+	x := "1234567891abcdefgh"
+	err = c.WritePacket([]byte(x))
+	if err != nil {
+		t.Fatal(err)
+	}
+	p, err := c.ReadPacket()
+	if string(p) != x {
+		t.Fatalf("Expected %s got %s", x, string(p))
+	}
+}
